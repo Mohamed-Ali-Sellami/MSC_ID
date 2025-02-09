f9dd0d1464ae3d8ce7c59cacffad2731797e6768
Mohamed-Ali-Sellami <medalisellamii@gmail.com>
2025-02-09 13:57:17 +0100

    version01

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -89,8 +89,8 @@ const Navbar = ({ hideAuthButtons }) => {
               <Link to="/solutions"> <div className="nav-item"><a href="#">Solutions</a></div></Link>
                 <div className="nav-item"><a href="#">MSC ID app</a></div>
                 <Link to="/verification"><div className="nav-item"><a href="#">Try for Free</a></div></Link>
-                <div className="nav-item"><a href="#">Company</a></div>
-                <div className="nav-item"><a href="#">Contact</a></div>
+                <Link to="/company"> <div className="nav-item"><a href="#">Company</a></div></Link>
+                <Link to="/contact"><div className="nav-item"><a href="#">Contact</a></div></Link>
                 
 
                 {isAuth ? (
